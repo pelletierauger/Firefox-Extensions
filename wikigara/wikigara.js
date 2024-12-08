334187513a8f5049209f03edb23d96c4b0ddb725
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2024-12-08 01:13:14 -0500

    Fix the punctuation on Wikipedia

--- wikigara/wikigara.js
+++ wikigara/wikigara.js
@@ -1,1 +1,36 @@
 document.body.style.fontFamily = "Adobe Garamond Pro";
+document.body.style.fontFamily = "EB Garamond 12 Regular";
+
+
+// max-width: 500px;
+// margin-left: auto;
+// margin-right: auto;
+
+
+let otherStyles = `.mw-body p {
+  margin: 0.5em 0 1em 0;
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}`;
+
+
+let styleSheet = document.createElement("style");
+styleSheet.innerText = otherStyles;
+document.head.appendChild(styleSheet);
+
+let text = document.querySelector(".mw-content-ltr");
+
+function replaceText(context, regEx, replace){
+    var childNodes = context.childNodes;
+    for (var i = 0; i<childNodes.length; i++) {
+        var childNode = childNodes[i];
+        if (childNode.nodeType === 3) // 3 is for text node
+            childNode.nodeValue = childNode.nodeValue.replace(regEx, replace);
+        else if (childNode.nodeType === 1 && childNode.nodeName != "HEAD")
+            replaceText(childNode, regEx, replace); 
+    }
+}
+replaceText(text, /'/ig, '’');
+replaceText(text, /([A-zÀ-ú,.])(")/ig, function(a, b, c){return b + '”'});
+replaceText(text, /(")([A-zÀ-ú,.])/ig, function(a, b, c){return '“' + c});
